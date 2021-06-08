bfdd19c23fdaa472a5a4c924f509a080676a4bb4
Jing Davies <78061103+jingd16@users.noreply.github.com>
2021-06-08 16:39:57 +1000

    home page, map working

--- static/app.js
+++ static/app.js
@@ -1,3 +1,4 @@
+//==============================country=====================
 var c_data="/country";
 
 d3.json(c_data, country_data=> {
@@ -5,6 +6,7 @@ d3.json(c_data, country_data=> {
     console.log(country_data);
 });
 
+//=============================hotel===========================
 var h_data="/hotel_reviews_cleaned";
 
 d3.json(h_data, hotel_data=> {
@@ -12,6 +14,7 @@ d3.json(h_data, hotel_data=> {
     console.log(hotel_data);
 });
 
+//============================restaurants=======================
 var r_data="/restaurants";
 
 d3.json(r_data, rests_data=> {
@@ -19,16 +22,50 @@ d3.json(r_data, rests_data=> {
     console.log(rests_data);
 });
 
-var cities_data="/cities";
 
-d3.json(cities_data, cities_data=> {
-    console.log("cities")
-    console.log(cities_data);
-});
-
+//==========================trains===============================
 var trains_data="/trains";
 
 d3.json(trains_data, t_data=> {
     console.log("train stations")
     console.log(t_data);
+});
+
+//============================cities=============================
+var cities_data="/cities";
+
+var myMap = L.map("map", {
+    center: [48.8566, 16.3522],
+    zoom: 4
+  });
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: "pk.eyJ1IjoiamluZ2QxNiIsImEiOiJja3Azc2dzbmEwMGl6Mm9yN2Jya2JnMnM1In0.IhXkxlhkjKEqwuhE-EI5cw"
+}).addTo(myMap);
+
+d3.json(cities_data, cities_data=> {
+    console.log("cities")
+    console.log(cities_data);
+
+    for (var i=0; i<cities_data.length; i++) {
+        
+        //var c=generateRandomColor();
+        if (cities_data[i].lat) {
+            var location = [cities_data[i].lat, cities_data[i].lng];
+            var popup_content = `<h1><strong>${cities_data[i].city_name}<strong></h1>
+                                <hr>
+                                <h3>${cities_data[i].city_country}</h3>
+                                <hr>
+                                <img src=${cities_data[i].city_url} alt="${cities_data[i].city_name} width="150" height="150"></img>`
+
+            L.marker(location)
+            .bindPopup(popup_content, {maxWidth: "auto"})
+            .addTo(myMap);
+        }
+    }
 });
